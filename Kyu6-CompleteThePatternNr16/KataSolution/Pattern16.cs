13eebbee88ee1d56d36e691277bae6e297f67e2b
Telpe <tommy.s.madsen@gmail.com>
2023-11-17 00:07:52 +0100

    Complete The Pattern #16 - Done

---
+++ Kyu6-CompleteThePatternNr16/KataSolution/Pattern16.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+
+namespace KataSolution
+{
+    public class Pattern16
+    {
+        public static string Pattern(int n)
+        {
+            if (1 > n) { return ""; }
+
+            char[] line = new char[n];
+            string[] lines = new string[n];
+
+            for (int i = 0; n > 0; n--, i++)
+            {
+                line[i] = n.ToString()[^1];
+
+                for (int iNext = i+1; iNext < line.Length; iNext++)
+                {
+                    line[iNext] = line[i];
+                }
+
+                lines[i] = new string(line);
+            }
+
+            // consider replacing '\n' with Environment.NewLine
+            return string.Join('\n', lines); 
+        }
+    }
+}
